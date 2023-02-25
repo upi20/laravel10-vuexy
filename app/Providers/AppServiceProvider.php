4dbbb87f0bcef6a7b6738beb448f95af72be4a44
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-02-25 12:43:35 +0700

    initial file

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // get all data from menu.json file
+        $verticalMenuJson = file_get_contents(base_path('resources/data/menu-data/verticalMenu.json'));
+        $verticalMenuData = json_decode($verticalMenuJson);
+        $horizontalMenuJson = file_get_contents(base_path('resources/data/menu-data/horizontalMenu.json'));
+        $horizontalMenuData = json_decode($horizontalMenuJson);
+
+        // Share all menuData to all the views
+        view()->share('menuData', [$verticalMenuData, $horizontalMenuData]);
+    }
+}
