e071aa5408e39b4cd8458d80c4308cba60263be9
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-02-26 10:38:09 +0700

    update view fogot password

--- resources/js/scripts/pages/auth-reset-password.js
+++ resources/js/scripts/pages/auth-reset-password.js
@@ -8,33 +8,39 @@
 ==========================================================================================*/
 
 $(function () {
-  'use strict';
+    'use strict';
 
-  var pageResetPasswordForm = $('.auth-reset-password-form');
+    var pageResetPasswordForm = $('.auth-reset-password-form');
 
-  // jQuery Validation
-  // --------------------------------------------------------------------
-  if (pageResetPasswordForm.length) {
-    pageResetPasswordForm.validate({
-      /*
-      * ? To enable validation onkeyup
-      onkeyup: function (element) {
-        $(element).valid();
-      },*/
-      /*
-      * ? To enable validation on focusout
-      onfocusout: function (element) {
-        $(element).valid();
-      }, */
-      rules: {
-        'reset-password-new': {
-          required: true
-        },
-        'reset-password-confirm': {
-          required: true,
-          equalTo: '#reset-password-new'
-        }
-      }
-    });
-  }
+    // jQuery Validation
+    // --------------------------------------------------------------------
+    if (pageResetPasswordForm.length) {
+        pageResetPasswordForm.validate({
+            /*
+            * ? To enable validation onkeyup
+            onkeyup: function (element) {
+              $(element).valid();
+            },*/
+            /*
+            * ? To enable validation on focusout
+            onfocusout: function (element) {
+              $(element).valid();
+            }, */
+            rules: {
+                'email': {
+                    required: true,
+                    email: true
+                },
+                'password': {
+                    required: true,
+                    minlength: 8,
+                },
+                'password_confirmation': {
+                    required: true,
+                    minlength: 8,
+                    equalTo: "#reset-password-new"
+                }
+            }
+        });
+    }
 });
